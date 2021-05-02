d4a7355f143d1ddbcc5facd73630f2e4e4446bf1
Zulqarnainhayder <zhayder816@gmail.com>
2021-05-02 06:29:44 -0400

    Update

--- src/main.js
+++ src/main.js
@@ -8,7 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBcl49EjAdBg_fx1zw6kXbVJl2Awpmk0Ko',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+  },
+})
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Halls.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id="top-section">
+      <div class="container">
+        <div class="row top-section-container-padding">
+          <div class="col-md-5">
+            <h1 class="top-section-main-heading">
+              Never Worry About Your Site Again
+            </h1>
+            <p class="top-section-text pt-5">
+              Let us handle the backend – we’ll manage and monitor your website
+              so it’s safe, secure, and always up-and-running.
+            </p>
+            <div class="pt-2">
+              <a class="btn btn-primary mr-3" href="#" role="button"
+                >Gets Started Now</a
+              >
+              <a class="btn btn-primary" href="#" role="button">Contact Us</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <GmapMap
+      :center="{ lat: 10, lng: 10 }"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 100%; height: 300px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GmapMap>
+  </div>
+</template>
